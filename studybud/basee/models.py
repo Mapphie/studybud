c8a5aa4a19d994af87d34ddb84bd3a9679c6e068
Mapphie <maphiesarobidyrazafindrakotoso@gmail.com>
2023-12-04 15:57:50 +0300

    messages

--- studybud/basee/models.py
+++ studybud/basee/models.py
@@ -9,21 +9,21 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     
     name = models.CharField(max_length=200)
-    description = models.TextField(null = True, blank=True)
-    #participants =
-    uptaded = models.DateTimeField(auto_now= True)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
+    updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        ordering = ['-created']
+        ordering = ['-updated','-created']
     
     def __str__(self):
-        return self.name 
-    
+        return self.name
+
 class Message(models.Model):
         user = models.ForeignKey(User, on_delete= models.CASCADE)
         room = models.ForeignKey(Room, on_delete = models.CASCADE)
